b6642b67c8b1d639d25b73a6fa532545110f95fa
etr217 <52478994+etr217@users.noreply.github.com>
2020-12-07 17:32:30 -0600

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -29,4 +29,4 @@ st.title('Streamlit Share And LIME Visualization')
 txt = st.text_area('sjifagsorejas', 'jieoaj')
 if st.button('Evaluate', key=None):
     c = make_pipeline(vectorizer, model)
-    components.html(c.predict(txt).as_html(), height=800)
+    components.html(c.predict(txt), height=800)
