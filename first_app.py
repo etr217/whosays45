8f6a26e743c69cfbe088fa539e8f636a1bd0ad59
etr217 <52478994+etr217@users.noreply.github.com>
2020-12-07 17:39:14 -0600

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -29,4 +29,4 @@ st.title('Streamlit Share And LIME Visualization')
 txt = st.text_area('sjifagsorejas', 'jieoaj')
 if st.button('Evaluate', key=None):
     c = make_pipeline(vectorizer, model)
-    st.write(c.predict_proba(txt))
+    st.write(c.predict_proba(str(txt)))
